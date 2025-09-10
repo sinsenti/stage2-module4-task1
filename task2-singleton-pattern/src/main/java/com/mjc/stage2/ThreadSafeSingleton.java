93509c1427abcbbbe956472e22d0989bfa0fd1d0
sinsenti <sinsenti73@gmail.com>
2025-09-10 12:02:34 +0300

    Auto-push: 2025-09-10 12:02:34

--- task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
+++ task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
@@ -1,7 +1,7 @@
 package com.mjc.stage2;
 
 public class ThreadSafeSingleton {
-  private static ThreadSafeSingleton instance = new ThreadSafeSingleton();
+  private volatile static ThreadSafeSingleton instance = new ThreadSafeSingleton();
 
   private ThreadSafeSingleton() {
   }
